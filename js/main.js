0603ac1b4e7898ba93134f49154544b2c4209b2e
Francesco Dente <francesco.dente2003@gmail.com>
2024-06-17 16:21:20 +0200

    1.0.1: build: structure complete, logic complete with prompts

--- js/main.js
+++ js/main.js
@@ -12,21 +12,38 @@ for (let i = 0; i < randomInts.length; i++) {
 
 element.innerHTML = randomInts;
 
+// faccio scomparire i numeri dalla pagina
+
+setTimeout(function() {
+   element.innerHTML = "";
+}, 28000);
+
 // prompts
 
 const userChoices = [];
 
 setTimeout(function() {
-   element.innerHTML = "";
-}, 2000);
-
-setTimeout(function() {
-   element.innerHTML = "";
    for (let i = 0; i < randomInts.length; i++) {
       userChoices[i] = parseInt(prompt(`inserisci il numero in posizione ${i + 1}` ));
    }
-}, 1000);
 
-for (let i = 0; i < randomInts.length; i++) {
+   const incorrect = [];
 
-}
+   let cnt = 0;
+
+   for (let i = 0; i < randomInts.length; i++) {
+      if (userChoices[i] === randomInts[i]) {
+         console.log(`numero in posizione ${i + 1} corretto`);
+         cnt++;
+      } else {
+         console.log(`numero in posizione ${i + 1} sbagliato`);
+         incorrect.push(`hai sbagliato in posizione ${i + 1}`);
+      }
+   }
+
+   if (cnt === randomInts.length) {
+      console.log("tutti corretti!");
+   } else {
+      console.log(incorrect);
+   }
+}, 30000);
